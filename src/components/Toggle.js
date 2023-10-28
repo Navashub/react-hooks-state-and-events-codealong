7b0e11d9fad2f5edb132bce790cb88df0b05300b
Navashub <navasmuller01@gmail.com>
2023-10-28 10:46:15 +0300

    did some changes on the components

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick(){
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+  <button style={{ background: color }} onClick={handleClick}>{isOn ? 'ON' : 'OFF'}</button>)
+  ;
 }
+
+// function Toggle() {
+//   const OFF = 'OFF';
+//   const [toggle, setToggle] = useState(OFF);
+//   function isToggle (){
+
+//     setToggle(toggle === OFF ? 'ON' : OFF);
+//   }
+
+//   return <button onClick={isToggle}>{toggle}</button>
+// }
 
 export default Toggle;
